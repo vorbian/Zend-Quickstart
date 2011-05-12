8f9704d9c185b6bd23467a6402df9a78fd720717
Vorbian <carusonau@gmail.com>
2011-05-12 15:28:09 -0500

    bootstrap and layout added

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,14 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+/*
+$frontController = Zend_Controller_Front::getInstance();
+$frontController->registerPlugin(new Initializer('development'));
+$router = $frontController->getRouter();
+$router->addRoute(
+	'test',
+	new Zend_Controller_Router_Route('/foo/:action/:id', array('controller'=>'foo'))
+	);
+	*/
 }
 
--- testcolor.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$x = array('red', 'blue', 'green', 'yellow');
-foreach ($x as $c) {
-	print "<br>this file is $c";
-}
-
